1b1edf77aeff0baccc152f55f329d422bada0565
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-01-04 17:30:34 +0530

    connect mongodb database
    connect mongodb database with the help of mongoose

--- server/src/app.js
+++ server/src/app.js
@@ -1,31 +1,45 @@
+const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // middleware
-const middleware = (req, res, next)=>{
+const middleware = (req, res, next) => {
     console.log("hello, i'm middleware");
     next();
 }
 
+const db = 'mongodb+srv://shreyash510:skcoder510@cluster0.xymkp.mongodb.net/mernstack?retryWrites=true&w=majority';
 
-app.get("/", (req, res)=>{
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+}).then(() => {
+    console.log("connection successful")
+}).catch((err) => {
+    console.log(err)
+})
+
+
+app.get("/", (req, res) => {
     res.send("hello")
 })
-app.get("/about", middleware, (req, res)=>{
+app.get("/about", middleware, (req, res) => {
     res.send("about")
 })
-app.get("/contact", (req, res)=>{
+app.get("/contact", (req, res) => {
     res.send("contact")
 })
-app.get("/signin", (req, res)=>{
+app.get("/signin", (req, res) => {
     res.send("login")
 })
-app.get("/signup", (req, res)=>{
+app.get("/signup", (req, res) => {
     res.send("resister")
 })
 
 
-app.listen(port, ()=>{
-    console.log("listening...");
+app.listen(port, () => {
+    console.log(`listening...${port}`);
 })
