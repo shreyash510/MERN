c43fba7398aa77d378286316bcaa72b490c56887
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-01-05 10:53:58 +0530

    store data database
    validation of email means email are already exit, so return and show error msg and store data in data base means user registered successfullly

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+require("../db/conn");
+const User = require("../model/userSchema");
+
 router.get("/", (req, res)=>{
     res.send("hello router")
 });
@@ -13,13 +16,25 @@ const {name, email,phone, work, password, cpassword} = req.body;
             return res.status(422).json({error: "plz filled the field properly"});
     }
 
-    // console.log(name);
-    // console.log(email);
-    // // res.json({message:req.body});
-});
+    User.findOne({email:email})
+    .then((userExist)=>{
+        if(userExist){
+            return res.status(422).json({
+                error: "email already exist"
+            });
+        }
+        const user = new User({name, email, phone, work ,password, cpassword})
+      user.save().then(()=>{
+          res.status(201).json({
+              message : "user registered successfully"
+          });
+      }).catch((err)=>{
+          res.status(500).json({error: "failed to register"})
+      })
+    }).catch((err)=>{
+        console.log(err)
+    })
 
-// router.get("/register", (req, res)=>{
-//     res.send("hello register");
-// })
+});
 
 module.exports = router;
