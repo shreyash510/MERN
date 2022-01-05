f85c35a2caf0e23d90b6b0aa57056e7d9fc300bc
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-01-05 16:16:03 +0530

    validation again
    means email and pass both are same or not

--- server/router/auth.js
+++ server/router/auth.js
@@ -5,63 +5,65 @@ const bcrypt = require('bcryptjs');
 require("../db/conn");
 const User = require("../model/userSchema");
 
-router.get("/", (req, res)=>{
+router.get("/", (req, res) => {
     res.send("hello router")
 });
 
 // user registration 
-router.post("/register", async (req, res)=>{
+router.post("/register", async (req, res) => {
 
-const {name, email,phone, work, password, cpassword} = req.body;
+    const { name, email, phone, work, password, cpassword } = req.body;
 
-    if(!name || !email || !phone || !work || !password || !cpassword){
-            return res.status(422).json({error: "plz filled the field properly"});
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "plz filled the field properly" });
     }
 
-    try{
-        const userExist = await User.findOne({email:email})
-            if(userExist){
-                return res.status(422).json({
-                    error: "email already exist"
-                });
-            }
-            const user = new User({name, email, phone, work ,password, cpassword})
-          
-            const userResister = await user.save();
+    try {
+        const userExist = await User.findOne({ email: email })
+        if (userExist) {
+            return res.status(422).json({
+                error: "email already exist"
+            });
+        }
+        const user = new User({ name, email, phone, work, password, cpassword })
 
-                if(userResister){
-                    return res.status(201).json({
-                        message : "user registered successfully"
-                    });
-                }else{
-                    res.status(500).json({error: "failed to register"})
-                }
-    }catch(err){
+        const userResister = await user.save();
+
+        if (userResister) {
+            return res.status(201).json({
+                message: "user registered successfully"
+            });
+        } else {
+            res.status(500).json({ error: "failed to register" })
+        }
+    } catch (err) {
         console.log(err)
     }
 
 });
 
 // user login 
-router.post("/login", async (req, res)=>{
- 
-    try{
-    const {email, password} = req.body;
-            if(!email || !password){
-                return res.status(400).json({error: "Plz fill the data"});
-            }
-                const userLogin =await User.findOne({email : email})
-                // console.log(userLogin);
+router.post("/login", async (req, res) => {
 
-                // varified hash password 
-                const isMatch = await bcrypt.compare(password, userLogin.password)
-                if(!isMatch) {
-                    res.json({error: "invalid email"})
-                }{
-                    res.json({message: "User signin Successfully"})
-                }
-            
-    }catch(err){
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "Plz fill the data" });
+        }
+        const userLogin = await User.findOne({ email: email })
+        // console.log(userLogin);
+        if (userLogin) {
+            // varified hash password 
+            const isMatch = await bcrypt.compare(password, userLogin.password)
+            if (!isMatch) {
+                res.json({ error: "invalid email" })
+            }else {
+                res.json({ message: "User signin Successfully" })
+            }
+        }else{
+            res.status(400).json({error : "Invalid creadients"});
+        }
+    } catch (err) {
         console.log(err)
     }
 })
