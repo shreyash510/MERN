960d38f08c75c7319222018dbf233f92967644d1
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-01-06 10:15:09 +0530

    create navbar

--- client/mern-app/src/componunts/Navbar.js
+++ client/mern-app/src/componunts/Navbar.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Navbar() {
     return (
         <>
-            <h1>hello this is navbar</h1>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href ="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto" >
+        <li className="nav-item ">
+          <a className="nav-link active" aria-current="page" href ="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href ="/">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href ="/">Contact</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href ="/">Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href ="/">Register</a>
+        </li>
+      </ul>      
+    </div>
+  </div>
+</nav>
         </>
     )
 }
